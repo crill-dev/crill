72b3a38816504ae0047beeb1a1aa0df98af1b228
Timur Doumler <mail@timur.audio>
2022-08-04 17:47:35 +0100

    Added doctest unit test framework

---
+++ tests/main.cpp
@@ -0,0 +1,7 @@
+// crill - the Cross-platform Real-time, I/O, and Low-Latency Library
+// Copyright (c) 2022 - Timur Doumler and Fabian Renn-Giles
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
