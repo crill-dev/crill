7d701959d2b984941c2e318355c3919daf73785c
Timur Doumler <mail@timur.audio>
2022-08-05 13:14:09 +0100

    Added platform detection macros; added stub for spin_mutex

---
+++ include/crill/spin_mutex.h
@@ -0,0 +1,13 @@
+// crill - the Cross-platform Real-time, I/O, and Low-Latency Library
+// Copyright (c) 2022 - Timur Doumler and Fabian Renn-Giles
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef CRILL_SPIN_MUTEX_H
+#define CRILL_SPIN_MUTEX_H
+
+#include <crill/platform.h>
+
+// TODO: implement
+
+#endif //CRILL_SPIN_MUTEX_H
---
+++ tests/spin_mutex_test.cpp
@@ -0,0 +1,9 @@
+// crill - the Cross-platform Real-time, I/O, and Low-Latency Library
+// Copyright (c) 2022 - Timur Doumler and Fabian Renn-Giles
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <doctest/doctest.h>
+#include <crill/spin_mutex.h>
+
+// TODO: write tests
